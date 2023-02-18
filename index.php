30624f392ce3d2649fe8c1f87db937b358178203
phojie rengel <phojrengel@gmail.com>
2023-02-18 19:56:57 +0800

    fix: set to vanilla php functionality

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
 
-Artisan::command('pldt {code}', function ($code) {
-	$table = [
-		'0' => 'f',
-		'1' => 'e',
-		'2' => 'd',
-		'3' => 'c',
-		'4' => 'b',
-		'5' => 'a',
-		'6' => '9',
-		'7' => '8',
-		'8' => '7',
-		'9' => '6',
-		'a' => '5',
-		'b' => '4',
-		'c' => '3',
-		'd' => '2',
-		'e' => '1',
-		'f' => '0',
-	];
+$code = (string) readline('Enter pldt default wifi code: ');
 
-	$code = str_split($code);
-	$code = array_map(function ($letter) use ($table) {
-		return $table[$letter];
-	}, $code);
+$table = [
+    '0' => 'f',
+    '1' => 'e',
+    '2' => 'd',
+    '3' => 'c',
+    '4' => 'b',
+    '5' => 'a',
+    '6' => '9',
+    '7' => '8',
+    '8' => '7',
+    '9' => '6',
+    'a' => '5',
+    'b' => '4',
+    'c' => '3',
+    'd' => '2',
+    'e' => '1',
+    'f' => '0',
+];
 
-	$code = implode('', $code);
+$code = str_split($code);
+$code = array_map(function ($letter) use ($table) {
+    return $table[$letter];
+}, $code);
 
-	$this->comment('wlan'.$code);
-})->purpose('Display PLDT password');
+$code = implode('', $code);
+
+echo "Password: wlan{$code}";
